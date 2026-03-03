6b9b6d5150acfe5e3eeeeb9137a6ee000a58cc7e
lila-vanilla <andrei.i.nedosekin@gmail.com>
2026-03-03 10:13:56 +0600

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,12 @@
+const products = [
+    {
+        id: 1,
+        category: "tops",
+        name: {
+            ru: "Гипюровый топ",
+            en: "Guipure Top"
+        },
+        price: 2200,
+        image: "images/top1.jpg"
+    }
+];

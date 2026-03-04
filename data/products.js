1c0e7602765ab29c34418db701db202b2ec1f238
lila-vanilla <andrei.i.nedosekin@gmail.com>
2026-03-04 10:31:58 +0600

    Update products.js

--- data/products.js
+++ data/products.js
@@ -8,6 +8,14 @@ const products = [
         },
         price: 2200,
         sizes: ["S", "M", "L"],
+        description: {
+            ru: "Нежный гипюровый топ с аккуратной посадкой. Подчёркивает силуэт.",
+            en: "Delicate guipure top with elegant fit. Highlights the silhouette."
+        },
+        care: {
+            ru: "Ручная стирка при 30°C. Не отбеливать.",
+            en: "Hand wash at 30°C. Do not bleach."
+        },
        images: [
             "images/top_01/top_01_01.jpg",
             "images/top_01/top_01_02.jpg",
